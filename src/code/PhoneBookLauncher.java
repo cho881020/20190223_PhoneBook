1b9035fe69e0dd66fa95f955a74a7ee8d9e8c119
soyoonyoon <starry55@naver.com>
2019-02-23 11:37:16 +0900

    로그인 기능 작성중
    - DB에 연결하는 부분까지!

--- src/code/PhoneBookLauncher.java
+++ src/code/PhoneBookLauncher.java
@@ -1,5 +1,10 @@
 package code;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class PhoneBookLauncher {
@@ -10,6 +15,7 @@ public class PhoneBookLauncher {
 		
 		Scanner scan = new Scanner(System.in);
 		
+		printMainMenu();
 //		0번을 누를때까지 무한반복
 		
 		while (true) {
@@ -19,6 +25,7 @@ public class PhoneBookLauncher {
 			int userInput = scan.nextInt();	
 			if (userInput == 1) {
 //				로그인	
+				login();
 			}
 			else if (userInput ==2) {
 //				회원가입
@@ -33,7 +40,7 @@ public class PhoneBookLauncher {
 			}
 		}
 		
-		printMainMenu();
+		
 	}
 
 //	메인화면을 출력하는 메쏘드
@@ -45,5 +52,67 @@ public class PhoneBookLauncher {
 		System.out.println("****************************");
 		System.out.println("원하는 동작을 입력 : ");
 		
+	}
+	
+//	아이디,비번 입력받아서 DB에 로그인시도
+	public void login() {
+//		아이디 입력받아서 저장
+		Scanner scan1 = new Scanner(System.in);
+		System.out.print("ID(Email) : ");
+		String userInputEmail = scan1.nextLine();
+		
+//		비밀번호 입력받아서 저장
+		Scanner scan2 = new Scanner(System.in);
+		System.out.print("Password : ");
+		String userInputPassword = scan2.nextLine();
+		
+//		JDBC를 통해서 DB서버에 실제 사용자가 맞는지 문의 .Query
+//		별개의 메쏘드가 수행한 결과를 로그인결과 변수에 저장.
+		boolean loginResult = findUserInDatabase(userInputEmail, userInputPassword);
+		
+		if (loginResult) {
+//			로그인 성공! 성공처리 메쏘드 별개로 작성
+			
+		}
+		else {
+			System.out.println("메인화면으로 이동합니다.");
+		}
+	}
+	
+//	아이디,비번을 가지고 실제로 DB에 있는지 쿼리를 날리는 메쏘드
+//	있으면 true, 없으면 false 를 리턴
+	public boolean findUserInDatabase(String email, String pw) {
+		boolean result = false;
+		
+//		DB와의 연결 상태를 저장하는 변수
+		Connection conn = null;
+//		연결된 디비에 쿼리를 실행시켜줌
+		Statement stmt = null;
+//		쿼리 실행 결과를 저장하는 변수 (표)
+		ResultSet rs = null;
+		
+
+		try {
+//			JDBC 불러오기
+			Class.forName("com.mysql.jdbc.Driver");
+//			불러온 JDBC 이용해 디비 접속
+//			접속 정보를 변수에 저장
+			String url = "jdbc:mysql://delivery.c0ctoatt9tr3.ap-northeast-2.rds.amazonaws.com/tjeit";
+			
+//			저장된 접속정보로 디비 접속
+			conn = DriverManager.getConnection(url, "delivery","dbpassword");
+			
+			System.out.println("DB연결 성공!");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return result;
 	}
 }
