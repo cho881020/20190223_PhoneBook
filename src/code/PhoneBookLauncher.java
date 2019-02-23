fbc4fe7b53ffbf5d6d341ac0aa4ce66e5bfb6984
monaru1124 <monaru1124@naver.com>
2019-02-23 11:48:16 +0900

    로그인 성공/실패 확인 작업
    실제로 데이터베이스에 쿼리를 날려서 확인

--- src/code/PhoneBookLauncher.java
+++ src/code/PhoneBookLauncher.java
@@ -77,9 +77,11 @@ public class PhoneBookLauncher {
 		
 		if(loginResult) {
 //			로그인 성공! 성공처리 메쏘드 별개로 작성.
+			System.out.println("로그인에 성공했습니다.");
 		}
 		
 		else {
+			System.out.println("아이디나 비번이 잘못되었습니다.");
 			System.out.println("메인화면으로 이동합니다.");
 		}
 		
@@ -110,17 +112,45 @@ public class PhoneBookLauncher {
 			String url = "jdbc:mysql://delivery.c0ctoatt9tr3.ap-northeast-2.rds.amazonaws.com/tjeit";
 			
 //			저장된 접속 정보와 아이디 비번을 가지고 실제로 DB에 접속.			
-			conn = DriverManager.getConnection(url, "deliver", "dbpassword");
+			conn = DriverManager.getConnection(url,"delivery","dbpassword");
 			
-			System.out.println("DB 연결 성공!");
-						
+//			재료로 받은 아이디/비번이 모두 맞는지 사용자가 있는지 쿼리.
+			String loginQuery = String.format("SELECT * FROM users "
+					+ "WHERE email = '%s' AND password = '%s';", email, pw);
+			
+//			쿼리를 수행해서 결과를 저장
+//			1. stmt 변수를 객체화
+			stmt = conn.createStatement();
+			
+//			2. stmt를 이용해서 loginQuery를 실행 + 결과를 rs에 저장
+			rs = stmt.executeQuery(loginQuery);
+			
+//			3. rs에 저장된 표를 조회
+//			rs.next는 다음 읽을 줄이 있다면 그 줄로 커서를 이동. true
+//			읽을 내용이 더 없다면 그냥 false를 리턴.
+			while(rs.next()) {
+//				이안에 들어왔다 => 아이디 비번이 맞는 사람이 있다!
+//				 => 로그인에 성공!
+				result = true;
+				
+				
+			}
+					
+		
+			
+			
 			
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("DB 드라이버 로딩 실패");
+			System.out.println("JDBC를 제대로 추가했는지 확인해주세요.");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("DB 서버 연결 실패");
+			System.out.println("DB 서버가 접속이 가능한 상태인지 확인.");
+			System.out.println("접속 정보를 틀리지 않고 입력했는지 코드 검토.");
 		}
 		
 		
