29a6e0efc63feee0eb8e6f64befa0bddc17de58b
Psojeong <6muse.on@gmail.com>
2019-02-23 11:48:27 +0900

    로그인 성공/실패 확인 성공
    실제로 데이터베이스에 쿼리를 날려서 확인

--- src/code/PhoneBookLauncher.java
+++ src/code/PhoneBookLauncher.java
@@ -1,5 +1,10 @@
 package code;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class PhoneBookLauncher {
@@ -17,6 +22,7 @@ public class PhoneBookLauncher {
 			
 			if (userInput == 1) {
 //				로그인
+				login();
 			}
 			else if (userInput == 2) {
 //				회원가입
@@ -43,6 +49,91 @@ public class PhoneBookLauncher {
 		System.out.println("***************************");
 		System.out.print("원하는 동작을 입력 : ");
 		
+	}
+	
+//	아이디, 비번을 입력받아서 DB에 로그인 시도
+	public void login() {
+		
+//		아이디를 입력받아서 저장
+		Scanner scan1 = new Scanner(System.in);
+		System.out.print( "ID(Email) : ");
+		String userInputEmail = scan1.nextLine();
+		
+//		비밀번호도 입력받아서 저장
+		Scanner scan2 = new Scanner(System.in);
+		System.out.print( "Password : ");
+		String userInputPassword = scan2.nextLine();
+		
+//		JDBC를 통해서 DB서버에 실제 사용자가 맞는지 문의. Query
+//		별개의 메쏘드가 수행한 결과를 로그인결과 변수에 저장
+		boolean loginResult = findUserInDatabase(userInputEmail, userInputPassword);
+		
+		if (loginResult) {
+//			로그인 성공! 성공 처리 메쏘드 별개로 작성.
+			System.out.println("로그인에 성공했습니다!");
+		}
+		else {
+			System.out.println("아이디나 비번이 잘못되었습니다.");
+			System.out.println("메인 화면으로 이동합니다.");
+		}
+				
+	}
+	
+//	아이디, 비번을 가지고 실제로 DB에 있는지 쿼리를 날리는 메쏘드
+//	있으면 true, 없으면 false를 리턴.
+	public boolean findUserInDatabase(String email, String pw) {
+		boolean result = false;
+		
+//		DB와의 연결상태를 저장해두는 변수
+		Connection conn = null;
+//		연결된 DB에 쿼리를 실행시켜줌.
+		Statement stmt = null;
+//		쿼리 실행 결과를 저장하는 변수. (표)
+		ResultSet rs = null;
+		
+
+		try {
+//			JDBC를 불러오기(try/catch 한거임)
+			Class.forName("com.mysql.jdbc.Driver");
+			
+//			불러온 JDBC를 이용해서 DB에 접속
+//			접속 정보를 변수에 저장.
+			String url = " jdbc:mysql://delivery.c0ctoatt9tr3.ap-northeast-2.rds.amazonaws.com/tjeit";
+			
+//			저장된 접속정보와 아이디 비번을 가지고 실제로 DB에 접속.
+			conn = DriverManager.getConnection(url, "delivery", "dbpassword");
+			System.out.println("DB 연결 성공!");
+			
+//			재료로 받은 아이디/비번이 모두 맞는 사용자가 있는지 쿼리.
+			String loginQuery = String.format("SELECT * FROM users "
+					+ "WHERE email = '%s' AND password = '%s';", email, pw);
+			
+//			쿼리를 수행해서 결과를 저장
+//			1. stmt 변수를 객체화
+			stmt = conn.createStatement();
+			
+//			2. stmt를 이용해서 loginQuery를 실행 + 결과를 rs에 저장
+			rs = stmt.executeQuery(loginQuery);
+			
+//			3. rs에 저장된 표를 조회
+//			rs.next는, 다음 읽을 줄이 있다면 그 줄로 커서를 이동. true
+//			읽을 내용이 더 없다면 그냥 false를 리턴
+			while (rs.next()) {
+//				이 안에 들어왔다 -> 아이디비번이 맞는 사람이 있다!
+//				 => 로그인에 성공!
+				result = true;
+				
+			}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 	
 
