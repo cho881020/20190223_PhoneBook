6b62bc1996ed21260d3d72f9469fde6f60d2509f
monaru1124 <monaru1124@naver.com>
2019-02-23 11:36:51 +0900

    로그인 기능 작성중
    - DB에 연결하는 부분까지

--- src/code/PhoneBookLauncher.java
+++ src/code/PhoneBookLauncher.java
@@ -1,5 +1,10 @@
 package code;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class PhoneBookLauncher {
@@ -18,7 +23,9 @@ public class PhoneBookLauncher {
 			int userInput = scan.nextInt();
 			
 			if(userInput == 1) {
-//				로그인
+//				로그인 => 다른 메쏘드로 이동(login())
+				login();
+				
 			}
 			
 			else if(userInput == 2) {
@@ -48,5 +55,99 @@ public class PhoneBookLauncher {
 		System.out.print(" 원하는 동작 입력 : ");
 		
 		
+	} // 프린트 메쏘드
+	
+	 
+//	아이디, 비번 입력받아서 DB에 로그인 시도
+	public void login() {
+		
+//		아이디를 입력받아서 저장
+		Scanner scan1 = new Scanner(System.in);
+		System.out.print("ID(Eamil) : ");
+		String userInputEmail = scan1.nextLine();
+		
+//		비밀번호도 입력받아서 저장
+		Scanner scan2 = new Scanner(System.in);
+		System.out.print("Password : ");
+		String userInputPassword = scan2.nextLine();
+		
+//		JDBC를 통해 DB 서버에 실제 사용자가 맞는지 문의. Query
+//		별개의 메쏘드가 수행한 결과를 로그인결과 변수에 저장.
+		boolean loginResult = findUserInDatabase(userInputEmail, userInputPassword);
+		
+		if(loginResult) {
+//			로그인 성공! 성공처리 메쏘드 별개로 작성.
+		}
+		
+		else {
+			System.out.println("메인화면으로 이동합니다.");
+		}
+		
 	}
+	
+//	아이디, 비번을 가지고 실제로 DB에 있는지 쿼리를 날리는 메쏘드.
+//	있으면 true, 없으면 false를 리턴.
+	
+	public boolean findUserInDatabase(String email, String pw) {
+		boolean result = false;
+		
+//		DB와의 연결 상태를 저장해두는 변수
+		Connection conn = null;
+		
+//		연결된 DB에 쿼리를 실행시켜줌.
+		Statement stmt = null;
+		
+//		쿼리 실행 결과를 저장하는 변수. (표)
+		ResultSet rs = null;
+		
+
+		try {
+//			JDBC를 불러오기.
+			Class.forName("com.mysql.jdbc.Driver");
+			
+//			불러온 JDBC를 이용해서 DB에 접속
+//			접속 정보를 변수에 저장.
+			String url = "jdbc:mysql://delivery.c0ctoatt9tr3.ap-northeast-2.rds.amazonaws.com/tjeit";
+			
+//			저장된 접속 정보와 아이디 비번을 가지고 실제로 DB에 접속.			
+			conn = DriverManager.getConnection(url, "deliver", "dbpassword");
+			
+			System.out.println("DB 연결 성공!");
+						
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return result;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
