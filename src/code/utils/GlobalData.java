b0c0ddd60d90807a254d4b61141332e96d031b1f
조경진 <kj_cho@idealidea.co.kr>
2019-02-23 12:11:25 +0900

    GlobalData를 이용해 로그인한 사용자 정보를 저장

---
+++ src/code/utils/GlobalData.java
@@ -0,0 +1,14 @@
+package code.utils;
+
+import code.datas.User;
+
+// 이 클래스에 만드는 변수는 모든 클래스에서 쉽게 접근하기 위한 목적
+
+public class GlobalData {
+	
+//	로그인한 사람의 정보를 담아두는 변수.
+//	null이라면 로그인한 사람이 없다!
+//	null 아니면 로그인한사람 정보 접근 가능.
+	public static User loginUser = null;
+
+}
