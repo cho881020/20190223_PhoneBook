8d95976c287b188c507d82855d35a57f2f041320
shairyu93 <shanav@naver.com>
2019-02-23 14:23:00 +0900

    user 클래스 수정

--- src/code/datas/User.java
+++ src/code/datas/User.java
@@ -5,8 +5,15 @@ public class User {
 //	로그인한 사용자와 이메일을 저장하는 메소드
 	private String email;
 	private String name;
+	private int id;
 	
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public String getEmail() {
 		return email;
 	}
